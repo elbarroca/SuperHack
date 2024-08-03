2c5dac47d1216344aa1c57706d8c5f73837ca17e
elbarroca <btcto154k@gmail.com>
2024-08-03 16:02:18 +0100

    Initial commit

---
+++ src/app/screens/screen1.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Screen1: React.FC = () => {
+  return (
+    <div>
+      <h1>Screen 1</h1>
+      <p>This is Screen 1</p>
+    </div>
+  );
+};
+
+export default Screen1;
---
+++ src/app/screens/screen2.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Screen2: React.FC = () => {
+  return (
+    <div>
+      <h1>Screen 2</h1>
+      <p>This is Screen 2</p>
+    </div>
+  );
+};
+
+export default Screen2;
